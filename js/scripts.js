a26751ead3b576984fc88f91e0d006f2d3da2f84
benchurchwell <ben@benchurchwell.com>
2023-01-18 19:27:53 -0800

    converts C to F with functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-function saySomething(whatToSay)  {
-  window.alert(whatToSay);
-}
-
+// business logic
 function add(number1, number2)  {
   return number1 + number2;
 }
+
+//user interface logic
+const number1 = parseInt(prompt("Enter a temperature in Celsius:"));
+// const number2 = prompt("Enter another number:");
+
+window.alert("The temperature in Fahrenheit is " + ((number1 * 1.8 + 32) + "F."));
