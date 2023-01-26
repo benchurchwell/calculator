e46389c3a8f61388a387034e8110143c610ee7d6
benchurchwell <ben@benchurchwell.com>
2023-01-25 20:25:28 -0800

    saved after debugger lesson

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,27 @@
-// business logic
-function add(number1, number2)  {
-  return number1 + number2;
+// Business Logic
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract(number1, number2) {
-  return number1 - number2;
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function multiply(number1, number2) {
-  return number1 * number2;
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divide(number1, number2) {
-  return number1 / number2;
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
-function CtoF(number1)  {
-  return number1 * 1.8 + 32
-}
-
-// user interface logic
-
+// User Interface Logic
 function handleCalculation(event) {
   event.preventDefault();
   const number1 = parseInt(document.querySelector("input#input1").value);
   const number2 = parseInt(document.querySelector("input#input2").value);
   const operator = document.querySelector("input[name='operator']:checked").value;
-  
+
   let result;
   if (operator === "add") {
     result = add(number1, number2);
@@ -41,7 +36,7 @@ function handleCalculation(event) {
   document.getElementById("output").innerText = result;
 }
 
-window.addEventListener("load", function()  {
-  const form = this.document.getElementById("calculator");
+window.addEventListener("load", function() {
+  const form = document.getElementById("calculator")
   form.addEventListener("submit", handleCalculation);
 });
